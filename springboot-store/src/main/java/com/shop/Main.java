a66996f23badb169bb3bafdfecda13441f77d008
lollipop6370 <knn101do@gmail.com>
2024-08-15 11:53:14 +0800

    sub

--- springboot-store/src/main/java/com/shop/Main.java
+++ springboot-store/src/main/java/com/shop/Main.java
@@ -1,7 +1,14 @@
 package com.shop;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.shop.mapper")
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args);
+
     }
 }
