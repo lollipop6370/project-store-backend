4fb9d9d4cab26ff3ba4601daa2853687e413ddf1
lollipop6370 <knn101do@gmail.com>
2024-08-15 18:15:23 +0800

    ok

---
+++ springboot-store/src/main/java/com/shop/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@RequestMapping("/home")
+@CrossOrigin
+public class HomeController {
+    
+}
